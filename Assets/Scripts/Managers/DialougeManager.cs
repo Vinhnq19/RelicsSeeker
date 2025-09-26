2dec29fe9812d42b889d86ab1914acf1bb09505e
Quang Vinh <nguyenquangvinh5219@gmail.com>
2025-09-26 11:19:08 +0700

    chore: first set up for project

---
+++ Assets/Scripts/Managers/DialougeManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class DialougeManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
